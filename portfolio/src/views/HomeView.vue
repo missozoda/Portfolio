fa2ddbb11bd33ee3b3f07be4cb56efc4aff41649
ozoda ernazarova <ozoshka1207@gmail.com>
2023-05-18 17:41:03 +0500

    add main temps

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
+  import Navbar from "../components/Navbar.vue";
+  import Hero from "../components/Hero.vue";
+  import Stacks from "../components/Stacks.vue";
+  import Projects from "../components/Projects.vue";
+  import Contact from "../components/Contact.vue";
+  import Footer from "../components/Footer.vue";
 
 </script>
 
 <template>
-  <main>
-
+  <main class="mx-auto px-32">
+    <Navbar/>
+    <Hero/>
+    <Stacks/>
+    <!-- <Projects/> -->
+    <!-- <Contact/> -->
+    <!-- <Footer/> -->
   </main>
 </template>
